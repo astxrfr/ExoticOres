df1c325f7ddbc97013eb020c73084bafc52b7f01
B-All19 <th3fant0m13@gmail.com>
2026-09-22 20:07:14 -0600

    Creación de clases

---
+++ src/main/java/net/kirks/exoticores/item/ThoriteArmorMaterial.java
@@ -0,0 +1,12 @@
+package net.kirks.exoticores.item;
+
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.crafting.Ingredient;
+import org.jetbrains.annotations.NotNull;
+
+public class ThoriteArmorMaterial implements ArmorMaterial {
+  private static final int[] HEALTH_PER_SLOT = new int[]{13, 15, 16, 11}
+  private static final int[] PROTECTION_PER_SLOT = new int[]{3, 8, 6, 3}
+}
